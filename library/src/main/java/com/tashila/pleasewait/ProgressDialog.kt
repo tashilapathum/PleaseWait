5d1ec5c900c9ce3652a1fa22e367be5c91bf3aa6
Tashila Pathum <tashila.pathum@gmail.com>
2023-04-07 22:36:50 +0530

    Handle orientation changes

--- library/src/main/java/com/tashila/pleasewait/ProgressDialog.kt
+++ library/src/main/java/com/tashila/pleasewait/ProgressDialog.kt
@@ -12,18 +12,29 @@ import androidx.fragment.app.FragmentActivity
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.tashila.pleasewait.databinding.DialogProgressBinding
 
-public class ProgressDialog(private val context: Context) : DialogFragment() {
+public class ProgressDialog() : DialogFragment() {
     private lateinit var binding: DialogProgressBinding
-    var title = ""
-    var message = ""
+    private var context: Context? = null
+    public var title = ""
+    public var message = ""
+
+    constructor(context: Context) : this() {
+        this.context = context
+    }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         binding = DialogProgressBinding.inflate(layoutInflater)
 
         //build dialog
-        val dialog = MaterialAlertDialogBuilder(context)
+        val dialog = MaterialAlertDialogBuilder(requireActivity())
             .setView(binding.root)
             .create()
+
+        // Restore saved state
+        if (savedInstanceState != null) {
+            title = savedInstanceState.getString(ARG_TITLE) ?: ""
+            message = savedInstanceState.getString(ARG_MESSAGE) ?: ""
+        }
 
         //set params
         if (title.isEmpty() and message.isEmpty())
@@ -39,6 +50,17 @@ public class ProgressDialog(private val context: Context) : DialogFragment() {
     public fun show() {
         val fragmentManager = (context as? FragmentActivity)?.supportFragmentManager
         show(fragmentManager!!, null)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(ARG_TITLE, title)
+        outState.putString(ARG_MESSAGE, message)
+    }
+
+    companion object {
+        private const val ARG_TITLE = "title"
+        private const val ARG_MESSAGE = "message"
     }
 
 }
